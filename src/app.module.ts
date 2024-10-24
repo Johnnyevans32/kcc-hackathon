a6778c7a7867f4a36d518a1cb95f198e06a41459
johnny evans <evanseburu32@gmail.com>
2024-10-24 22:25:39 +0100

    kcc issuance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { IDVService } from './idc.service';
+import { IDVService } from './idv.service';
 
 @Module({
   imports: [],
